0944ba94e14f197c4be0703514286888d8860c03
Daniel Schmid <mail+git@ybc37.dev>
2023-10-30 10:43:17 +0100

    visidata: add config

---
+++ visidata/.config/visidata/config.py
@@ -0,0 +1,9 @@
+options.clipboard_copy_cmd = "wl-copy"
+options.clipboard_paste_cmd = "wl-paste"
+
+options.color_status = "black on 245"
+options.color_active_status = "black on 245"
+options.color_key_col = "magenta"
+options.color_keystrokes = "black on 245"
+options.color_menu = "black on 245"
+options.color_menu_help = "black italic on 109"
