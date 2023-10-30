65cf51a6a9f8031eb42e18dd7c9370234bf412af
Daniel Schmid <mail+git@ybc37.dev>
2023-10-30 11:17:11 +0100

    nvim: remove unnecessary config for illuminate

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -48,9 +48,6 @@ local custom_attach = function(client)
 
   illuminate = require('illuminate')
   illuminate.on_attach(client)
-  vim.api.nvim_command [[ hi def link LspReferenceText CursorLine ]]
-  vim.api.nvim_command [[ hi def link LspReferenceWrite CursorLine ]]
-  vim.api.nvim_command [[ hi def link LspReferenceRead CursorLine ]]
   map('n', '<a-n>', '<cmd>lua illuminate.next_reference{ wrap=true }<CR>')
   map('n', '<a-p>', '<cmd>lua illuminate.next_reference{ reverse=true, wrap=true }<CR>')
 end
