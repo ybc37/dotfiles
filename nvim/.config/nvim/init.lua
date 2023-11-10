c4d8891d77d001dce4b702fec687cd16c54ec47a
Daniel Schmid <mail+git@ybc37.dev>
2023-11-10 13:04:33 +0100

    nvim: remove packer workaround for folding

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,19 +23,8 @@ vim.opt.smartcase = true
 vim.opt.scrolloff = 3
 
 -- Folds based on tree-sitter (see workaround below)
--- vim.opt.foldmethod = 'expr'
--- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-
--- WORKAROUND
--- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
-vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
-  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-  callback = function()
-    vim.opt.foldmethod     = 'expr'
-    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
-  end
-})
--- ENDWORKAROUND
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 
 -- Sets 'foldlevel' when starting to edit another buffer in a window.
 vim.opt.foldlevelstart = 99
