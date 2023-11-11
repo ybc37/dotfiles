77739e02e767f66772e26fa023000f81492e3b89
Daniel Schmid <mail+git@ybc37.dev>
2023-11-11 20:45:50 +0100

    nvim: add nvim-web-devicons as dependency for telescope.nvim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
     config = function()
       local telescope = require('telescope')
       local actions = require('telescope.actions')
