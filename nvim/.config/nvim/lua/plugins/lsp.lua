9d571754dd728b4869e418a264ecd838f5c2c01a
Daniel Schmid <mail+git@ybc37.dev>
2023-11-12 01:06:18 +0100

    nvim: use icons for lsp diagnostics in sign columns

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -174,6 +174,12 @@ return {
         update_in_insert = true,
         severity_sort = true,
       })
+
+      local signs = { Error = '󰅚 ', Warn = '󰀪 ', Hint = '󰌶 ', Info = ' ' }
+      for type, icon in pairs(signs) do
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
     end,
   },
 }
