b77a0e1fa02cec76b1055f83415e634dc919aa20
Daniel Schmid <mail+git@ybc37.dev>
2024-08-18 17:38:14 +0200

    nvim: remove unnecessary nesting in leap.nvim spec

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,22 +1,20 @@
 return {
-  {
-    'ggandor/leap.nvim',
-    config = function()
-      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, '<leader>S', '<Plug>(leap-from-window)')
+  'ggandor/leap.nvim',
+  config = function()
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, '<leader>S', '<Plug>(leap-from-window)')
 
-      require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+    require('leap').opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
 
-      vim.keymap.set({ 'n', 'x', 'o' }, 'ga', function()
-        require('leap.treesitter').select()
-      end)
+    vim.keymap.set({ 'n', 'x', 'o' }, 'ga', function()
+      require('leap.treesitter').select()
+    end)
 
-      vim.keymap.set({ 'n', 'x', 'o' }, 'gA', 'V<cmd>lua require("leap.treesitter").select()<cr>')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'gA', 'V<cmd>lua require("leap.treesitter").select()<cr>')
 
-      vim.keymap.set({ 'n', 'o' }, '<leader>s', function()
-        require('leap.remote').action()
-      end)
-    end,
-  },
+    vim.keymap.set({ 'n', 'o' }, '<leader>s', function()
+      require('leap.remote').action()
+    end)
+  end,
 }
