d8210860b0bd7a013657359840220454141b204f
Daniel Schmid <mail+git@ybc37.dev>
2024-08-18 13:17:35 +0200

    nvim: fix closing floating windows (exclude focusable)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -166,7 +166,7 @@ vim.keymap.set('n', '<Leader><Esc>', '<C-w><C-o>')
 vim.keymap.set('n', '<Esc>', function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local config = vim.api.nvim_win_get_config(win)
-    if config.relative ~= '' then -- `relative` is empty for normal windows.
+    if config.relative ~= '' and config.focusable then -- `relative` is empty for normal windows.
       vim.api.nvim_win_close(win, false)
     end
   end
