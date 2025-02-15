73ead27d6f1d1553e388bdf2111e27dc2268f8a2
Daniel Schmid <mail+git@ybc37.dev>
2025-02-15 16:00:53 +0100

    nvim: remove multiline threshold of nvim-treesitter-context

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -95,7 +95,6 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     config = function()
       require('treesitter-context').setup({
-        multiline_threshold = 5,
         separator = '-',
       })
 
