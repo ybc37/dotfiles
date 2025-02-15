547a0317ed7cb674f0a43e5428e296b1705c216e
Daniel Schmid <mail+git@ybc37.dev>
2025-02-15 16:07:59 +0100

    nvim: add oil.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -88,4 +88,19 @@ return {
     event = 'InsertEnter',
     config = true,
   },
+
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local oil = require('oil')
+
+      oil.setup({
+        delete_to_trash = true,
+      })
+
+      vim.keymap.set('n', '<Leader>o', oil.open, { noremap = true })
+    end,
+  },
 }
