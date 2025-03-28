79202aa468079169230594f47d8e8111c446aabb
Daniel Schmid <mail+git@ybc37.dev>
2025-03-28 18:38:34 +0100

    nvim: use vim.diagnostic.config() to define signs (sign_define() is deprecated with 0.11)

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -182,13 +182,15 @@ return {
       vim.diagnostic.config({
         update_in_insert = true,
         severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '● ',
+            [vim.diagnostic.severity.WARN] = '● ',
+            [vim.diagnostic.severity.INFO] = '● ',
+            [vim.diagnostic.severity.HINT] = '● ',
+          },
+        },
       })
-
-      local signs = { Error = '● ', Warn = '● ', Hint = '● ', Info = '● ' }
-      for type, icon in pairs(signs) do
-        local hl = 'DiagnosticSign' .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl })
-      end
     end,
   },
 }
