9288e9b7fd34b61955184c2aa8b048934bea466c
Daniel Schmid <mail+git@ybc37.dev>
2025-07-11 15:21:05 +0200

    nvim: improve mappings of oil.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -92,6 +92,16 @@ return {
 
       oil.setup({
         delete_to_trash = true,
+
+        -- Enhances to default keymaps
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+          ['<C-s>'] = { 'actions.select', opts = { horizontal = true } },
+          ['<Esc>'] = { 'actions.close', mode = 'n' },
+          ['<C-l>'] = false,
+          ['<C-r>'] = 'actions.refresh',
+        },
       })
 
       vim.keymap.set('n', '-', oil.open, { noremap = true })
