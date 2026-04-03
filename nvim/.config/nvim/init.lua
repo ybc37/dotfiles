89fdbac0845003774f9abfd40ec12b8b14f579fd
Daniel Schmid <mail+git@ybc37.dev>
2026-04-03 22:27:36 +0200

    nvim: improve lazy config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -235,6 +235,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+  ui = {
+    border = 'single',
+    backdrop = 100,
+  },
+})
 
 vim.cmd.colorscheme('gruvbox')
